bed1d76d8d9acc64846781985c3ee32c51bd0497
humin <1106805538@qq.com>
2021-11-04 11:43:11 +0800

    map control

--- components/restaurantDeviceBigCard/index.vue
+++ components/restaurantDeviceBigCard/index.vue
@@ -1,32 +1,27 @@
 <template>
   <view class="restaurant-card" @click="handleClick">
     <view class="head">
-      <view class="name bold">老妈兔头</view>
+      <view :class="`name bol`" :style="{color: titleColor}">老妈兔头</view>
       <view class="right">
-        <view class="head-tips">
-          <image src="/static/icon/dot-green.png" style="height: 10rpx;width: 10rpx"></image>已安装在线监控
-        </view>
         <image class="right-icon" src="/static/icon/right.png"></image>
       </view>
     </view>
     <view class="detail">
       <view class="detail-item">
-        <view>
-          <image class="detail-icon" src="/static/icon/water-tiny.png"></image>
+        <view class="icon">
+          <image class="detail-icon" src="/static/icon/water.png"></image>
         </view>
         <view class="device-status">
           <text>当前浓度</text>
           <text class="status">35</text><text class="unit">&nbsp;mg/m³</text>
         </view>
-      </view>
-      <view class="detail-item right">
         <view class="device-status">
           <text>超标情况</text>
-          <text class="status green bold">正常</text>
+          <text class="status">正常</text>
         </view>
       </view>
       <view class="detail-item">
-        <view>
+        <view  class="icon">
           <image class="detail-icon" src="/static/icon/leaf-tiny.png"></image>
         </view>
         <view class="device-status">
@@ -39,7 +34,7 @@
         </view>
       </view>
       <view class="detail-item right">
-        <view>
+        <view  class="icon">
           <image class="detail-icon" src="/static/icon/fan-tiny-off.png"></image>
         </view>
         <view class="device-status">
@@ -57,7 +52,13 @@
 </template>
 <script>
 export default {
-  name:'RestaurantDeviceCard',
+  name:'RestaurantDeviceBigCard',
+  props:{
+    titleColor: {
+      type: String,
+      default: "#1083fd"
+    }
+  },
   data(){
     return {}
   },
@@ -70,50 +71,23 @@ export default {
 </script>
 <style scoped lang="scss">
 .restaurant-card {
-  padding: 24rpx 22rpx 29rpx 25rpx;
+  padding: 27rpx 26rpx 30rpx 26rpx;
   box-shadow: 0 2rpx 6rpx 0 rgba(21, 21, 21, 0.08);
   border-radius: 10rpx;
   border: solid 1rpx #e9edf1;
 
   background-color: #FFFFFF;
 
-  .detail {
-    display: flex;
-    justify-content: space-between;
-    flex-wrap: wrap;
-  }
-
-  .detail-icon {
-    height: 26rpx;
-    width: 26rpx;
-    margin-right: 7rpx;
-  }
-
-  .device-status {
-    font-size: 22rpx;
-    line-height: 34rpx;
-    letter-spacing: 0;
-    color: #808080;
-
-    .status {
-      color: #0b0b0b;
-      margin-left: 10rpx;
-
-      &.off {
-        color: #fd0909;
-      }
-    }
-
-    &:not(:last-child) {
-      margin-right: 19rpx;
-    }
-  }
+  width: 702rpx;
 
   .head {
-    margin-bottom: 42rpx;
+    margin-bottom: 28rpx;
     display: flex;
     justify-content: space-between;
-    border-bottom: 2rpx dashed #e9edf1;
+
+    .name {
+      font-size: 30rpx;
+    }
 
     .head-tips {
       font-size: 22rpx;
@@ -125,13 +99,13 @@ export default {
         margin-right: 8rpx;
       }
     }
-    
+
     .right {
       display: flex;
       justify-content: center;
       align-items: center;
     }
-    
+
     .name {
       font-size: 26rpx;
       line-height: 34rpx;
@@ -148,10 +122,56 @@ export default {
   }
 
   .detail {
+    display: flex;
+    justify-content: space-between;
+    padding: 33rpx 30rpx 29rpx 31rpx;
+
+    box-shadow: 0 2rpx 6rpx 0 rgba(21, 21, 21, 0.08);
+    border-radius: 10rpx;
+    border: solid 1rpx #e9edf1;
+  }
+
+  .detail-icon {
+    height:63rpx;
+    width: 63rpx;
+    margin-bottom: 22rpx;
+  }
+
+  
+  .device-status {
+    font-size: 22rpx;
+    line-height: 34rpx;
+    letter-spacing: 0;
+    color: #808080;
+
+    .status {
+      color: #0b0b0b;
+      margin-left: 10rpx;
+
+      &.off {
+        color: #fd0909;
+      }
+    }
+
+    .unit {
+      font-size: 14rpx;
+      
+    }
+
+    &:not(:last-child) {
+      margin-right: 19rpx;
+    }
+  }
+
+  .detail {
     .detail-item {
       display: flex;
       align-content: center;
-      flex-basis: 50%;
+      flex-direction: column;
+
+      .icon {
+        align-self: center;
+      }
 
       &.right{
         display: flex;
